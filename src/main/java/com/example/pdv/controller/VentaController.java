c2a041f9f2784b587eddace19818b0e454483a71
Enzo Sierra <enzogsierra@gmail.com>
2022-05-07 11:47:26 -0300

    Se añadió estilos BS5 y herencia de plantillas

--- src/main/java/com/example/pdv/controller/VentaController.java
+++ src/main/java/com/example/pdv/controller/VentaController.java
@@ -3,15 +3,17 @@ package com.example.pdv.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 @Controller
-public class HomeController
+@RequestMapping("/ventas")
+public class VentaController 
 {
     @GetMapping(value = {"", "/"})
     public String home(Model model)
     {
-        model.addAttribute("title", "Gestión de Punto de Ventas - Dashboard");
-        return "home";
-    }
+        model.addAttribute("title", "Listado de ventas");
+        return "ventas/listado";
+    }    
 }
